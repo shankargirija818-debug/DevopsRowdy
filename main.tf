3f50a8fa4baf25502776b28e17adafd76b53e7a4
Dynamicxpath <lenkagirija3@gmail.com>
2025-12-29 12:46:40 +0530

    made some changes as per locals and conditionals

--- main.tf
+++ main.tf
@@ -12,6 +12,35 @@
 # }
 
 ##### Variables#################
+# provider "aws" {
+#     region = var.aws_region
+
+  
+# }
+# data "aws_ami" "amazon_linux" {
+#     most_recent = true
+#     owners=["amazon"]
+#     filter {
+#         name   = "name"
+#         values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+#     }
+
+#     filter {
+#         name   = "virtualization-type"
+#         values = ["hvm"]
+#     }
+
+# }
+# resource "aws_instance" "my_ec2_instance" {
+#     ami           = data.aws_ami.amazon_linux.id
+#     instance_type = var.instance_type
+#     tags = {
+#       Name = var.instance_name
+# }
+
+# }
+
+##Conditionals and Locals#################
 provider "aws" {
     region = var.aws_region
 
@@ -31,11 +60,15 @@ data "aws_ami" "amazon_linux" {
     }
 
 }
+locals {
+  name_tag=var.instance_type=="t3.micro" ? "Micro Instance":"Standard Instance"
+
+}
 resource "aws_instance" "my_ec2_instance" {
     ami           = data.aws_ami.amazon_linux.id
     instance_type = var.instance_type
     tags = {
-      Name = var.instance_name
+      Name = local.name_tag
 }
 
 }
--- output.tf
+++ output.tf
@@ -1,4 +1,9 @@
-output "instance_public_ip" {
-  value = aws_instance.my_ec2_instance.public_ip
+# output "instance_public_ip" {
+#   value = aws_instance.my_ec2_instance.public_ip
+
+# }
+####Conditionals and Locals#################
+output "instance_name_tag" {
+  value = local.name_tag
 
 }
